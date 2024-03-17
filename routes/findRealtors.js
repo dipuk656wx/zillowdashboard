e33dc3e95de182acbcb4828077b9a2d9018fa662
dipuk656wx <97152857+dipuk656wx@users.noreply.github.com>
2024-03-17 13:25:18 +0530

    update

--- routes/findRealtors.js
+++ routes/findRealtors.js
@@ -4,8 +4,18 @@ const multer = require('multer');
 const xlsx = require('xlsx');
 const router = express.Router();
 const upload = multer();
-
-
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilio_client = require('twilio')(accountSid, authToken);
+const senderNumber = process.env.SENDERMOBILENUMBER;
+twilio_client.api.accounts(accountSid)
+  .fetch()
+  .then(account => {
+    console.log('Twilio authentication successful. Account SID:', account.sid);
+  })
+  .catch(error => {
+    console.error('Twilio authentication failed:', error);
+  });
 router.get("/find", async (req, res) => {
     try {
         const { by, keyword } = req.query;
@@ -18,7 +28,6 @@ router.get("/find", async (req, res) => {
         condition[by] = { $regex: keyword, $options: 'i' };
 
         const result = await Realtor.find(condition);
-        console.log(result);
         res.json(result);
     } catch (error) {
         console.error('Error finding realtors:', error);
@@ -54,6 +63,30 @@ router.post('/upload', upload.single('excelFile'), async(req, res) => {
         res.status(500).send('Internal server error.');
     }
 });
+router.post("/sendMessage", async (req, res) => {
+    const { message, numbers } = req.body;
+    try {
+        const promises = numbers.map(number => {
+            return twilio_client.messages.create({
+                from: senderNumber,
+                body: message,
+                to: "+917091702233"
+            });
+        });
 
+        Promise.all(promises)
+            .then(messages => {
+                messages.forEach(message => console.log(message.sid));
+                res.status(200).send('Messages sent successfully.');
+            })
+            .catch(error => {
+                console.error('Error sending messages:', error);
+                res.status(500).send('Error sending messages.');
+            });
+    } catch (error) {
+        console.error('Error sending messages:', error);
+        res.status(500).send('Error sending messages.');
+    }
+});
 
 module.exports = router;
